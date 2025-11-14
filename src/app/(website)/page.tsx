0126237aeb9fea0690f8aa86944ba0be819b9355
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-11-14 06:06:06 +0600

    feat: add new reusable sections for products and services, update homepage with video background
    - Added MostPopularProducts, OurProducts, OurServices, and WorkingProcess components.
    - Updated the homepage to include new sections for products and services.
    - Introduced a video background in the Banner component.
    - Added new assets: banner video and product image.
    - Updated package.json with new dev dependencies for commit linting and prettier.

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,9 +1,17 @@
+import MostPopularProducts from "@/components/ReusableSection/MostPopularProducts";
+import OurProducts from "@/components/ReusableSection/OurProducts";
+import OurServices from "@/components/ReusableSection/OurServices";
+import WorkingProcess from "@/components/ReusableSection/WorkingProcess";
 import Banner from "@/components/website/PageSections/HomePage/Banner";
 
 export default function page() {
   return (
     <div>
       <Banner />
+      <WorkingProcess />
+      <OurProducts />
+      <OurServices />
+      <MostPopularProducts />
     </div>
   );
 }
