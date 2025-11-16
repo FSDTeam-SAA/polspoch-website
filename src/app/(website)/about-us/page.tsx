a0d2dd1fed34efaf3264b16c1c6f76f13ba8e669
iftykhar <s.m.ifty49@gmail.com>
2025-11-16 06:58:47 +0600

    fix: navbar and footer

---
+++ src/app/(website)/about-us/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div >
+        <div className="container mx-auto">
+            <h2 className="">
+                About Us Page
+            </h2>
+            <div>
+                <h2 className="text-2xl">
+                    Who We Are
+                </h2>
+                <p className="mt-4 text-gray-600">
+                    We are a leading company in our industry, committed to providing top-notch products and services to our valued customers. Our team of dedicated professionals works tirelessly to ensure customer satisfaction and excellence in every aspect of our business.
+                </p>
+            </div>
+            
+        </div>
+    </div>
+
+  );
+}
