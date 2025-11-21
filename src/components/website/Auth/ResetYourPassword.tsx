0b318747b1790fcb05168de3c7e7271a8ed6b145
iftykhar <s.m.ifty49@gmail.com>
2025-11-21 06:41:11 +0600

    feat: [200~feat(auth): add reset password and email verification pages

--- src/components/website/Auth/ResetYourPassword.tsx
+++ src/components/website/Auth/ResetYourPassword.tsx
@@ -1,34 +1,98 @@
+// import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+// import { Eye } from "lucide-react";
+// import React from "react";
+
+// export default function ResetYourPassword() {
+//   return (
+
+//     <div className="w-full max-w-md ">
+//       {/* Title */}
+//       <h1 className="text-4xl font-semibold mb-2">Reset Your Password</h1>
+//       <p className="text-gray-500 text-sm mb-8">
+//         Enter your email address and we&apos;ll send you code to reset your
+//         password.
+//       </p>
+
+//       {/* Email */}
+//       <div className="mb-4">
+//         <label className="block text-sm mb-1">Email Address</label>
+//         <Input
+//           placeholder="hello@example.com"
+//           className="bg-transparent border border-gray-700 text-gray-800 h-12"
+//         />
+//       </div>
+
+//       {/* Send Code  Button */}
+//       <Button className="w-full h-12 cursor-pointer bg-[#8a1f0b] hover:bg-[#8a1f0bcc] text-white rounded-md text-[16px]">
+//         Send Code
+//       </Button>
+//     </div>
+//   );
+// }
+
+
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Eye } from "lucide-react";
-import React from "react";
+import { Label } from "@/components/ui/label";
+import useAuth from "@/lib/hooks/useAuth";
 
 export default function ResetYourPassword() {
+  const [email, setEmail] = useState("");
+
+  const router = useRouter();
+
+  const { loading, handleForgotPassword } = useAuth();
+
+  const handleSendCode = async () => {
+    const response = await handleForgotPassword(email);
+
+    if (response.success && response.data?.data?.accessToken) {
+      // Get the accessToken
+      const accessToken = response.data.data.accessToken;
+      router.push(`/verify-otp?token=${encodeURIComponent(accessToken)}`);
+    }
+  };
+
   return (
-    // <div className="min-h-screen flex items-center justify-center bg-transparent px-4 py-10">
-
-    // </div>
+    // <div className="min-h-screen flex items-center justify-center bg-[#faf8f6] p-4">
     <div className="w-full max-w-md ">
-      {/* Title */}
-      <h1 className="text-4xl font-semibold mb-2">Reset Your Password</h1>
-      <p className="text-gray-500 text-sm mb-8">
-        Enter your email address and we&apos;ll send you code to reset your
-        password.
-      </p>
+      {" "}
+      <div className=" ">
+        {" "}
+        <h1 className="text-3xl font-bold text-gray-900">
+          Reset Your Password{" "}
+        </h1>
+        <p className="text-gray-500 mt-1 text-sm">
+          Enter your email address and we’ll send you a code to reset your
+          password.{" "}
+        </p>
+        <div className="mt-6 space-y-4">
+          <div>
+            <Label>Email Address</Label>
 
-      {/* Email */}
-      <div className="mb-4">
-        <label className="block text-sm mb-1">Email Address</label>
-        <Input
-          placeholder="hello@example.com"
-          className="bg-transparent border border-gray-700 text-white h-12"
-        />
+            <Input
+              type="email"
+              className="mt-1 py-5"
+              placeholder="hello@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <Button
+            className="w-full bg-[#8A1B00] hover:bg-[#701600] mt-4 text-white cursor-pointer"
+            onClick={handleSendCode}
+            disabled={loading}
+          >
+            {loading ? "Sending..." : "Send Code"}
+          </Button>
+        </div>
       </div>
-
-      {/* Send Code  Button */}
-      <Button className="w-full h-12 cursor-pointer bg-[#8a1f0b] hover:bg-[#8a1f0bcc] text-white rounded-md text-[16px]">
-        Send Code
-      </Button>
     </div>
   );
 }
