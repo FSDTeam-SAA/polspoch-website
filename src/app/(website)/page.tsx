f7f35074ff4d31f042b30db959b464e27ef75145
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2026-03-02 06:38:50 +0600

    feat: integrate Google Analytics tracking scripts across multiple components

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -5,10 +5,26 @@ import Review from "@/components/ReusableSection/Review";
 import ShippingPolicy from "@/components/ReusableSection/Shippingpolicy";
 import WorkingProcess from "@/components/ReusableSection/WorkingProcess";
 import Banner from "@/components/website/PageSections/HomePage/Banner";
+import Script from "next/script";
 
 export default function page() {
   return (
     <div>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-GCQDY36ZF3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GCQDY36ZF3');
+          `}
+        </Script>
+      </head>
       <Banner />
       <WorkingProcess />
       <OurProducts />
